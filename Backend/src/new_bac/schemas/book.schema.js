f586e62486612016e5c2b6b29bd96624dc8da787
“darkblackv32” <angello.soldi@utec.edu.pe>
2023-07-01 12:30:48 -0500

    Problemas al crear el libros, relacionado al TOKEN

---
+++ Backend/src/new_bac/schemas/book.schema.js
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export const createBookSchema = z.object({
+    author: z.string({
+      required_error: "Title is required",
+    }),
+    tile : z.string({
+        required_error: "Title is required",
+    }),
+    status: z.string().optional(),
+    date: z.string().datetime().optional(),
+  });
+  
