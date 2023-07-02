bc2189f48f1ba8ffab573d175a3bf8061749d481
Roger <rhuauyam@uni.pe>
2023-07-01 19:18:49 -0500

    hello world added

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ import morgan from 'morgan';
 import cors from 'cors';
 import authRoutes from './routes/auth.routes.js';
 import cookieParser from 'cookie-parser';
-import tasks_route from './routes/books.routes.js'
+import booksRoutes from "./routes/books.routes.js";
 
 const app = express();
 
@@ -12,9 +12,11 @@ app.use(express.json());
 app.use(morgan('dev'));
 app.use(cookieParser())
 
-
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 app.use('/auth', authRoutes);
-app.use('/tasks', tasks_route);
+app.use('/books', booksRoutes);
 
 
 export default app;
